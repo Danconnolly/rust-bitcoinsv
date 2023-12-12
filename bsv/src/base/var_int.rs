eb64ed1efd80aabd64e629b3cbde73639f96c178
Daniel Connolly <daniel@dconnolly.com>
2023-12-12 01:02:05 +0100

    change VarInt to keep track of raw value

--- bsv/src/base/var_int.rs
+++ bsv/src/base/var_int.rs
@@ -4,56 +4,74 @@ use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 /// The VarInt Bitcoin data type with async serialization.
 /// Code based on https://github.com/brentongunning/rust-sv
 pub struct VarInt {
-    pub value: u64
+    pub value: u64,
+    pub raw: Vec<u8>,
 }
 
 impl VarInt {
     pub fn new(v: u64) -> VarInt {
         VarInt {
-            value: v
+            value: v,
+            raw: VarInt::raw_from_v(v),
         }
     }
 
     pub fn size(&self) -> usize {
-        return if self.value <= 252 {
-            1
-        } else if self.value <= 0xffff {
-            3
-        } else if self.value <= 0xffffffff {
-            5
-        } else {
-            9
-        };
+        return self.raw.len();
     }
 
     ///
     /// Writes the var int to bytes
     pub async fn write<R: AsyncWrite + Unpin>(&self, writer: &mut R) -> io::Result<()> {
-        if self.value <= 252 {
-            writer.write_u8(self.value as u8).await.unwrap();
-        } else if self.value <= 0xffff {
-            writer.write_u8(0xfd).await.unwrap();
-            writer.write_u16_le(self.value as u16).await.unwrap();
-        } else if self.value <= 0xffffffff {
-            writer.write_u8(0xfe).await.unwrap();
-            writer.write_u32_le(self.value as u32).await.unwrap();
-        } else {
-            writer.write_u8(0xff).await.unwrap();
-            writer.write_u64_le(self.value).await.unwrap();
-        }
+        writer.write_all(&self.raw).await.unwrap();
+        // if self.value <= 252 {
+        //     writer.write_u8(self.value as u8).await.unwrap();
+        // } else if self.value <= 0xffff {
+        //     writer.write_u8(0xfd).await.unwrap();
+        //     writer.write_u16_le(self.value as u16).await.unwrap();
+        // } else if self.value <= 0xffffffff {
+        //     writer.write_u8(0xfe).await.unwrap();
+        //     writer.write_u32_le(self.value as u32).await.unwrap();
+        // } else {
+        //     writer.write_u8(0xff).await.unwrap();
+        //     writer.write_u64_le(self.value).await.unwrap();
+        // }
         Ok(())
     }
 
     /// Reads a var int from bytes
     pub async fn read<R: AsyncRead + Unpin>(reader: &mut R) -> io::Result<VarInt> {
         let n0 = reader.read_u8().await.unwrap();
-        Ok( VarInt {
-            value: match n0 {
+        let v = match n0 {
             0xff => reader.read_u64_le().await.unwrap(),
             0xfe => reader.read_u32_le().await.unwrap() as u64,
             0xfd => reader.read_u16_le().await.unwrap() as u64,
-            _ => n0 as u64,
-        }})
+            _ => n0 as u64 };
+        Ok( VarInt {
+            value: v,
+            raw: VarInt::raw_from_v(v),
+        })
+    }
+
+    fn raw_from_v(v: u64) -> Vec<u8> {
+        match v {
+            0..=252 => vec![v as u8],
+            253..=0xffff => {
+                let mut o = vec![0xfd];
+                o.extend_from_slice(&(v as u16).to_le_bytes());
+                o
+            }
+            0x10000..=0xffffffff => {
+                let mut o = vec![0xfe];
+                o.extend_from_slice(&(v as u32).to_le_bytes());
+                o
+            }
+            _ => {
+                let mut o = vec![0xff];
+                o.extend_from_slice(&v.to_le_bytes());
+                o
+            }
+        }
     }
 }
 
