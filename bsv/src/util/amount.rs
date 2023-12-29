35d507287131b44b9f27a92ade9b78f279278ff9
Daniel Connolly <daniel@dconnolly.com>
2023-12-29 23:20:22 +0100

    add Amount for representing amounts in BSV

---
+++ bsv/src/util/amount.rs
@@ -0,0 +1,67 @@
+use core::fmt;
+use std::fmt::Write;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+/// Amount in BSV format, with 10^8 satoshis per BSV.
+#[derive(Debug, PartialEq)]
+pub struct  Amount(pub i64);
+
+impl Amount {
+    /// The zero amount.
+    pub const ZERO: Amount = Amount(0);
+    /// Exactly one satoshi.
+    pub const ONE_SAT: Amount = Amount(1);
+    /// Exactly one bitcoin.
+    pub const ONE_BSV: Amount = Amount(100_000_000);
+
+    /// Convert to a float, using 1BSV = 10^8 satoshis. Dont use this in calculations.
+    pub fn as_bsv(&self) -> f64 {
+        self.0 as f64 / 100_000_000.0
+    }
+}
+
+impl fmt::Display for Amount {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let bsv = self.as_bsv();
+        let mut s = format!("{:.8}", bsv);
+        while s.ends_with('0') {
+            s.pop();
+        }
+        if s.ends_with('.') {
+            s.push('0');
+        }
+        f.write_str(&s)
+    }
+}
+
+impl Serialize for Amount {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error> where S: Serializer {
+        Ok(f64::serialize(&self.as_bsv(), serializer)?)
+    }
+}
+
+impl<'de> Deserialize<'de> for Amount {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error> where D: Deserializer<'de> {
+        let bsv = f64::deserialize(deserializer)?;
+        Ok(Amount((bsv * 100_000_000.0) as i64))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn json_serialize_amount() {
+        let amount = Amount(100_000_000);
+        let json = serde_json::to_string(&amount).unwrap();
+        assert_eq!(json, "1.0");
+    }
+
+    #[test]
+    fn json_deserialize_amount() {
+        let json = "1.0";
+        let amount: Amount = serde_json::from_str(json).unwrap();
+        assert_eq!(amount, Amount(100_000_000));
+    }
+}
