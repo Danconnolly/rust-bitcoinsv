64cbb720b58eac10e85eb4ef2c8d7f5e4d8c7a88
Daniel Connolly <daniel@dconnolly.com>
2024-01-01 16:26:59 +0100

    publish FullBlockStream

--- bsv/src/lib.rs
+++ bsv/src/lib.rs
@@ -4,5 +4,5 @@ mod util;
 mod result;
 
 pub use result::{Error, Result};
-pub use bitcoin::{Blockchain, BlockHash, BlockHeader, Encodable, Hash, MerkleRoot, Outpoint, Tx, TxHash, TxInput, TxOutput, VarInt};
+pub use bitcoin::{Blockchain, BlockHash, BlockHeader, Encodable, FullBlockStream, Hash, MerkleRoot, Outpoint, Tx, TxHash, TxInput, TxOutput, VarInt};
 pub use util::Amount;
