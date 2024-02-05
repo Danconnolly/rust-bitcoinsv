0098b3478378bcb666ca4085d3627d73c7bb94b5
Daniel Connolly <daniel@dconnolly.com>
2024-02-05 17:22:06 +0100

    ensure that reader & writer get updated config.

--- bsv/src/p2p/stream.rs
+++ bsv/src/p2p/stream.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 use log::{info, trace, warn};
 use tokio::net::TcpStream;
 use tokio::sync::mpsc::{channel, Receiver, Sender};
+use tokio::sync::RwLock;
 use tokio::task::JoinHandle;
 use crate::p2p::{ACTOR_CHANNEL_SIZE, PeerAddress};
 use crate::p2p::config::CommsConfig;
@@ -57,8 +58,8 @@ struct PeerStreamActor {
     stream_state: StreamState,                        // current state of the stream
     peer: PeerAddress,
     conn_config: Arc<ConnectionConfig>,                 // the desired connection configuration
-    config: CommsConfig,                               // the active configuration for the stream
-    data_channel: P2PMessageChannelSender,               // P2P Data messages are sent on this channel
+    config: Arc<RwLock<CommsConfig>>,                   // the active configuration for the stream
+    data_channel: P2PMessageChannelSender,              // P2P Data messages are sent on this channel
     writer_rx: Option<Receiver<P2PMessage>>,
     writer_tx: Sender<P2PMessage>,
     reader_rx: Receiver<Arc<P2PMessage>>,
@@ -66,6 +67,7 @@ struct PeerStreamActor {
     version_received: bool,                             // true if we have received a version message
     verack_received: bool,                              // true if we have received a verack message in response to our version
     send_headers: bool,                                 // has peer requested we send headers
+    relay_tx: bool,                                     // has peer requested we relay transactions
 }
 
 impl PeerStreamActor {
@@ -79,11 +81,12 @@ impl PeerStreamActor {
             inbox: receiver, peer: peer_address,
             stream_state: StreamState::Starting,
             conn_config,
-            config: comms_config,
+            config: Arc::new(RwLock::new(comms_config)),
             data_channel, writer_rx: Some(writer_rx), writer_tx, reader_rx, reader_tx,
             version_received: false,
             verack_received: false,
             send_headers: false,
+            relay_tx: true,         // default is true, the peer can request not to relay tx
         };
         p.main().await;
     }
@@ -133,8 +136,11 @@ impl PeerStreamActor {
     async fn handle_received(&mut self, msg: Arc<P2PMessage>) {
         match self.stream_state {
             StreamState::Handshaking => {
-                match *msg {
-                    P2PMessage::Version(_) => {
+                match &*msg {
+                    P2PMessage::Version(v) => {
+                        { let mut c = self.config.write().await;
+                        c.protocol_version = v.version; }
+                        self.relay_tx = v.relay;
                         let va = P2PMessage::Verack;
                         self.send_msg(va).await;
                         self.version_received = true;
@@ -164,7 +170,8 @@ impl PeerStreamActor {
                         match &*msg {
                             P2PMessage::Protoconf(p) => {
                                 // we can send larger messages to the peer
-                                self.config.max_send_payload_size = p.max_recv_payload_length as u64;
+                                let mut c = self.config.write().await;
+                                c.max_send_payload_size = p.max_recv_payload_length as u64;
                             },
                             P2PMessage::SendHeaders => {
                                 // we should send headers
@@ -199,11 +206,12 @@ impl PeerStreamActor {
     // The writer task. It continually reads from the channel and writes to the socket.
     // It has not state, it just reads and writes what it is given. In particular, it does not check the message
     // size.
-    async fn writer(mut rx: Receiver<P2PMessage>, mut writer: tokio::net::tcp::OwnedWriteHalf, config: CommsConfig) {
+    async fn writer(mut rx: Receiver<P2PMessage>, mut writer: tokio::net::tcp::OwnedWriteHalf, shared_config: Arc<RwLock<CommsConfig>>) {
         trace!("writer task started.");
         loop {
             match rx.recv().await {
                 Some(msg) => {
+                    let config = shared_config.read().await.clone();
                     match msg.write(&mut writer, &config).await {
                         Ok(_) => {}
                         Err(e) => {
@@ -220,9 +228,10 @@ impl PeerStreamActor {
 
     // The reader task. It continually reads from the socket and writes to the channel.
     // It has no state or intelligence, it just reads and writes.
-    async fn reader(tx: Sender<Arc<P2PMessage>>, mut reader: tokio::net::tcp::OwnedReadHalf, config: CommsConfig) {
+    async fn reader(tx: Sender<Arc<P2PMessage>>, mut reader: tokio::net::tcp::OwnedReadHalf, shared_config: Arc<RwLock<CommsConfig>>) {
         trace!("reader task started.");
         loop {
+            let config = shared_config.read().await.clone();
             match P2PMessage::read(&mut reader, &config).await {
                 Ok(msg) => {
                     match tx.send(Arc::new(msg)).await {
@@ -243,8 +252,8 @@ impl PeerStreamActor {
     // Send initial configuration messages after the handshake
     async fn send_config(&mut self) {
         // maybe send the protoconf message
-        if self.conn_config.max_recv_payload_size > DEFAULT_MAX_PAYLOAD_SIZE {
-            let protoconf = Protoconf::new(self.config.max_recv_payload_size as u32);
+        if self.conn_config.max_recv_payload_size > DEFAULT_MAX_PAYLOAD_SIZE && self.conn_config.max_recv_payload_size <= u32::MAX as u64 {
+            let protoconf = Protoconf::new(self.conn_config.max_recv_payload_size as u32);
             let protoconf_msg = P2PMessage::Protoconf(protoconf);
             self.send_msg(protoconf_msg).await;
         }
