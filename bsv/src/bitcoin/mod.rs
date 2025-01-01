c7b2054e7cd77a51f0a2a5d921aad9c9c84289a8
Daniel Connolly <daniel@dconnolly.com>
2025-01-01 01:18:20 +0000

    expose keyaddresskind

--- bsv/src/bitcoin/mod.rs
+++ bsv/src/bitcoin/mod.rs
@@ -20,7 +20,7 @@ pub use self::crypto::{PrivateKey, PublicKey};
 pub use self::encoding::{AsyncEncodable, Encodable};
 pub use self::hash::Hash;
 pub use self::header::{BlockHash, MerkleRoot, BlockHeader};
-pub use self::params::BlockchainId;
+pub use self::params::{BlockchainId, KeyAddressKind};
 pub use self::script::*;
 pub use self::tx::{TxHash, Tx, TxInput, TxOutput, Outpoint, TxBuilder};
 pub use self::var_int::{varint_size, varint_decode, varint_encode};
