b20439a1698a3e3e2a90758ecc7fce009885fd73
Daniel Connolly <daniel@dconnolly.com>
2025-04-04 09:43:14 +0000

    doc improvement

--- bsv/src/bitcoin/encoding.rs
+++ bsv/src/bitcoin/encoding.rs
@@ -2,9 +2,6 @@ use crate::Result;
 use bytes::{Buf, BufMut};
 
 /// Read & write Bitcoin data structures to and from binary in Bitcoin encoding format.
-///
-/// This is the non-async trait for reading and writing to buffers. See [AsyncEncodable] for the
-/// async trait to read and write with truly async sources.
 pub trait Encodable {
     /// Read the data structure from a buffer.
     fn from_binary(buffer: &mut dyn Buf) -> Result<Self>
@@ -16,7 +13,7 @@ pub trait Encodable {
 
     /// Return the size of the encoded form.
     // It is vital (for efficiency) that implementations of this function use a method that does not just encode the object
-    // and count the bytes. This is because this function is used to determine the size of the buffer to allocate
+    // and count the bytes. This is because this function can be used to determine the size of the buffer to allocate
     // for the encoding.
     fn encoded_size(&self) -> u64;
 }
